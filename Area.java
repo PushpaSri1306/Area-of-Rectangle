636799dcdd4cd69734fb9053a799ed1b92db285d
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 20:14:48 +0530

    Area of a rectangle

---
+++ Area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        float area = a*b;
+        System.out.println(area);
+        sc.close(); 
+    }
+}
